a55025654c5e858a8ef0947c5ad90ae1273c9bea
Aleksandr Sesyolkin <sesyolkin@gmail.com>
2019-03-30 21:46:56 +0300

    Updates

---
+++ precache-manifest.598baea6b8af3b369c51d4584a3fc2bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a26f159a46b92e0be32",
+    "url": "/react-quiz/static/js/runtime~main.8a26f159.js"
+  },
+  {
+    "revision": "16e063c4ff269f2aa0c7",
+    "url": "/react-quiz/static/js/main.16e063c4.chunk.js"
+  },
+  {
+    "revision": "0af28a808303d7bb6219",
+    "url": "/react-quiz/static/js/2.0af28a80.chunk.js"
+  },
+  {
+    "revision": "16e063c4ff269f2aa0c7",
+    "url": "/react-quiz/static/css/main.24ff10f9.chunk.css"
+  },
+  {
+    "revision": "b799546938f45120bb875dc9fe3e5929",
+    "url": "/react-quiz/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a14212c1eacdb0b2c698560f45da5a49.js"
+  "/react-quiz/precache-manifest.598baea6b8af3b369c51d4584a3fc2bb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-quiz/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
